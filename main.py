fb3e04e704ad3de0d566e96bdb0ab6eeb255b66d
Dan Coleman <32960483+DanSoulman@users.noreply.github.com>
2018-12-06 16:54:12 +0000

    Broke the code down into functions

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import re
 
+#Reads in data, splits up each word and makes lowercase  
+def readFile(src):
+    text_file = open(src) 
+    read_file = text_file.read() #Variable holding the full unedited tweets
+    word_list = read_file.lower().split() #Splits up each word seperately into all lowercase
+    return word_list
+    
+
 #Filters out the keys that aren't allowed
 def formatWords(word_list):
 
@@ -10,66 +18,51 @@ def formatWords(word_list):
     return list(filter(lambda cursedWord: not allowedWords.search(cursedWord) ,word_list))  
 
 
-#Goes through the positive, negative and whole word list and counts the occurance of each word in each
-def wordCounter(clean_list, clean_list2, fullList, posDictionary, negDictionary, fullDictionary):
-    
-    #--------------------------------------------------------
-    #G E T T I N G  O C C U R A N C E  O F  E A C H  W O R D
-    #--------------------------------------------------------
-
-    #Positive wordcount
+#Counts the times each word appears, sets it as the value for the dictionary passed in
+def wordOccurance(clean_list, dictionary):
     for word in clean_list: #For each word in clean_list 
-        posDictionary[word] = posDictionary[word]+1 #Increments key by one  
+        dictionary[word] = dictionary[word]+1 #Increments key by one  
     #For testing     
-    #print("The positive dictionary:\n" , posDictionary) #Prints the dictionary and its occurances 
+    #print("The positive dictionary:\n" , dictionary) #Prints the dictionary and its occurances 
     #input("Hit enter to continue:") #This is to break up information into readable chunks
+    return dictionary
 
-    #Negative wordcount
-    for word in clean_list2: 
-        negDictionary[word] = negDictionary[word]+1  
-    #For testing
-    #print("The negative dictionary:\n" , negDictionary)  
-    #input("Hit enter to continue:") 
-
-    #Combined wordcount
-    for word in clean_list: #For each word in clean_list 
-        fullDictionary[word] = fullDictionary[word]+1 
-    for word in clean_list2: #Then also adds in clean_list2
-        fullDictionary[word] = fullDictionary[word]+1
-    #For Testing
-    #print("The whole dictionary:\n" , fullDictionary)  
-    #input("Hit enter to continue:") 
-
-   
-    #----------------------------------------------------------------------------------------------------
-    #G E T T I N G  P R O B A B I L I T Y  O F  A  W O R D  B E I N G  P O S I T I V E / N E G A T I V E
-    #----------------------------------------------------------------------------------------------------
+#Works out from the full dictionary the probability any given word is positive
+def positivityOfTrainingWords(posDictionary, fullDictionary):
     vocabPos = {} #Dictionary that stores the probability that a word is positive as its value
-    finalVerdict = {} #Dictionary that stores if the algorithm thinks the tweet is positive or negative
-
     for word in fullDictionary:
         fOccurance = fullDictionary[word]
         posProbability = (posDictionary[word]/fOccurance)
         #negProbability = (1 - posProbability) #Got negative occurance until I realised I didn't need it 
         vocabPos[word] = posProbability
-
         #For testing
         #print(f"The probablility that the word {word} is Positive: {vocabPos[word]} Negative: {(1 - vocabPos[word])}") 
     #input("Hit enter to continue:")
-   
+    return vocabPos
+
+
+#It reads in the test data we were given, finds each word from our dictionary and works out from that how likey a tweet is to be positive or negative
+def predictTweets(posDictionary, negDictionary, fullDictionary, vocabPos):
+    
     #-------------------------------------------
     #Assigning the likelyhood of a word being Positive/Negative
     #-------------------------------------------
+    finalVerdict = {} #Dictionary that stores if the algorithm thinks the tweet is positive or negative
+   
     #Opens in file seperated by tweets
-    tweetsPos = open("dataFiles/test/testPos.txt", 'r').read().lower().split("@") 
-    tweetsNeg = open("dataFiles/test/testNeg.txt", 'r').read().lower().split("@")
+    tweetsPos = open("dataFiles/test/testPos.txt", 'r').read().lower().split("\n") 
+    tweetsNeg = open("dataFiles/test/testNeg.txt", 'r').read().lower().split("\n")
+    #Combines full list of tweets
     tweets = tweetsPos + tweetsNeg 
+
+    #Initialise varibles used to count how many are predicted positive or negative
+    postiveTweets, negativeTweets = 0,0
+
     #Loops through words in the tweet to find any trained to be positive/negative
     for tweet in tweets:
         tweetWords = re.split(r"[^\w]", tweet) #Should clean up the special characters. TODO: Test this is properly fundtioning
-
         probOfPos, count, probability = 0,0,0 #Initialise varibles needed foe the loop
-        
+           
         #Loops through each word in the tweet, if it appeared in the trainer it goes through and grabs the probability. Then adds that to the total probability and ups counted words
         for word in tweetWords:
             if vocabPos.__contains__(word):
@@ -78,57 +71,79 @@ def wordCounter(clean_list, clean_list2, fullList, posDictionary, negDictionary,
                 probability =(probOfPos/count) #works out average probability 
                 #print("The probablility that the whole tweet is Positive is" , probability) #for testing 
         
-
+        #Assigns each tweet Positive or Negative
         if probability > 0.5:
             finalVerdict[tweet] = "Positive"
+            postiveTweets +=1 
             #print(f"{tweet} is positive")
         elif probability < 0.5:
             finalVerdict[tweet] = "Negative"
+            negativeTweets += 1
             #print(f"{tweet} is negative")
         else:
             finalVerdict[tweet] = "Undecided"
-    return finalVerdict
-        
+    #print(postiveTweets, negativeTweets)
 
-def accuracyCheck(verdict):
-    for key, value in verdict.items():
-        print(key,value)
+    #-----------------------------------------------
+    #Getting Accuracy
+    #-----------------------------------------------
+    positiveCount = len(tweetsPos)
+    negativeCount = len(tweetsNeg)
+    accuracyPos = ((postiveTweets/(positiveCount+negativeCount))*100)
+    accuracyNeg = ((negativeTweets/(positiveCount+negativeCount))*100)
+    print(f"The positive accuracy is {accuracyPos} \nThe negative accuracy is {accuracyNeg}")    
+
 
 #Reads in the File and sorts it into the dictionary. 
 def main():
     #-------------------------------------------
     #READING IN DATA & SETTING UP DICTIONARY : 
-    #TODO: If there is more time make this a fuction
     #-------------------------------------------
-    #Opens text file in read only
-    #text_file = open("dataFiles/test/new_test.txt", 'r')  
-    text_file = open("dataFiles/train/trainPos.txt", 'r') 
+    #Opens text file in read only  
+    posList = readFile("dataFiles/train/trainPos.txt")
+    negList = readFile("dataFiles/train/trainNeg.txt")
     
-    read_file = text_file.read() #Variable holding the full unedited tweets
-    word_list = read_file.lower().split() #Splits up each word seperately into all lowercase
-    clean_list = formatWords(word_list) #Gets rid of special characters
-    uniquewords = set(clean_list) #Creates a set of just unique words from the list of words
-
-    #Does it again for the 2nd file
-    #text_file2 = open("dataFiles/test/new_test2.txt", 'r')
-    #text_file2 = open("dataFiles/test/testNeg.txt", 'r') 
-    text_file2 = open("dataFiles/train/trainNeg.txt", 'r') 
-    read_file2 = text_file2.read() 
-    word_list2 = read_file2.lower().split()
-    clean_list2 = formatWords(word_list2) 
-    uniquewords2 = set(clean_list2) 
+    #Gets rid of special characters
+    cleanPosList = formatWords(negList) 
+    cleanNegList = formatWords(posList)
+    
+    #Creates a set of just unique words from the list of words
+    uniquePos = set(cleanPosList) 
+    uniqueNeg = set(cleanNegList)
     
     #Makes a full list of words from Positive and Negative
-    fullList = uniquewords2|uniquewords
+    fullSet = uniquePos|uniqueNeg
 
     #Creates a dictionary setting the value of each fullList to 0 for counting
-    posDictionary = dict.fromkeys(fullList, 0) 
-    negDictionary = dict.fromkeys(fullList, 0)
-    fullDictionary = dict.fromkeys(fullList, 0)
+    posDictionary = dict.fromkeys(fullSet, 0) 
+    negDictionary = dict.fromkeys(fullSet, 0)
+    fullDictionary = dict.fromkeys(fullSet, 0)
     
-    #Calls on the wordCounter
-    verdict = wordCounter(clean_list, clean_list2 ,fullList, posDictionary, negDictionary, fullDictionary) 
-    accuracyCheck(verdict)
+    #--------------------------------------------------------
+    #GETTING OCCURANCE OF EACH WORD
+    #--------------------------------------------------------
+    posDictionary = wordOccurance(cleanPosList, posDictionary)
+    negDictionary = wordOccurance(cleanNegList, negDictionary)
+    #Combined wordcount
+    for word in cleanPosList: #For each word in clean_list 
+        fullDictionary[word] = fullDictionary[word]+1 
+    for word in cleanNegList: #Then also adds in clean_list2
+        fullDictionary[word] = fullDictionary[word]+1
+    
+    #----------------------------------------------------------------------------------------------------
+    #GETTING PROBABILITY OF A WORD BEING POSITIVE / NEGATIVE
+    #----------------------------------------------------------------------------------------------------
+    vocabPos = positivityOfTrainingWords(posDictionary, fullDictionary)
+
+
+    #-----------------------------------------------------------------------------------
+    #PREDICT IF TWEETS ARE POSITIVE OR NEGATIVE
+    #-----------------------------------------------------------------------------------
+    predictTweets(posDictionary, negDictionary, fullDictionary, vocabPos) 
+        
+    
+
+
 main()
 
 
